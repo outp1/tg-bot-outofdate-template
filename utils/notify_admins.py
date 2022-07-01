83fa06c7ecf7e7ad75bf9e80240d6dba6f48a4d5
mdar <81954928+outp1@users.noreply.github.com>
2022-07-01 14:45:53 +0300

    Delete utils directory

--- utils/notify_admins.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from aiogram import Dispatcher
-
-from data import config
-
-async def on_startup_notify(dp: Dispatcher):
-    for admin in config.ADMINS:
-        await dp.bot.send_message(admin, f"Бот Запущен")
